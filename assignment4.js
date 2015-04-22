afedae3f726785a142cfd19b24ca3aa8ee039acd
jholkeboer <jholkeboer@users.noreply.github.com>
2015-04-22 00:41:03 -0700

    form designed, next step ajax

--- assignment4.js
+++ assignment4.js
@@ -1,1 +1,12 @@
 //assignment4.js
+
+function gistAjax() {
+	var req = new XMLHttpRequest();
+	if (!req) {
+		throw 'Request did not go through.'
+	}
+}
+
+window.onload = function() {
+	gistAjax();
+}
