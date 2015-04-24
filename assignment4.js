cb80e0b85d536ce1c2306fb404a990e3fe4ed293
jholkeboer <jholkeboer@users.noreply.github.com>
2015-04-23 22:41:27 -0700

    CSS layout

--- assignment4.js
+++ assignment4.js
@@ -1,12 +1,32 @@
 //assignment4.js
 
+function testFunc(printInput) {
+	document.getElementById('output').innerHTML = 'test';
+}
+
 function gistAjax() {
 	var req = new XMLHttpRequest();
 	if (!req) {
-		throw 'Request did not go through.'
+		throw 'Request did not go through.';
 	}
+	//var url = 
+}
+
+function saveDemoInput() {
+	localStorage.setItem('demoText',document.getElementsByName('demo-input')[0].value);
+}
+
+function clearLocalStorage() {
+	localStorage.clear();
+}
+
+function displayLocalStorage(fav) {
+	document.getElementById('output').innerHTML = localStorage.getItem('demoText');
 }
 
 window.onload = function() {
+	testFunc("Gwalla gwalla");
+	saveDemoInput();
+	displayLocalStorage("swag");
 	gistAjax();
-}
+};
