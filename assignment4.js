13fea91e07bb3f30c68154ebe9a6859dba5e9256
jholkeboer <jholkeboer@users.noreply.github.com>
2015-04-24 00:05:57 -0700

    working on ajax

--- assignment4.js
+++ assignment4.js
@@ -1,32 +1,65 @@
 //assignment4.js
+//jack holkeboer
 
-function testFunc(printInput) {
-	document.getElementById('output').innerHTML = 'test';
+function printGists() {
+
 }
 
-function gistAjax() {
+//takes dictionary of parameters and puts them in an array to be added to the url
+function appendToQuery(params) {
+	var queryPiece = [];
+	for (var key in params) {
+		var tempString = encodeURIComponent(key) + '=' + encodeURIComponent(params[key]);
+		queryPiece.push(tempString);
+	}
+	return queryPiece.join('&');
+}
+
+function gistAjax(numPages, python, javascript, sql, jason) {
 	var req = new XMLHttpRequest();
 	if (!req) {
 		throw 'Request did not go through.';
 	}
-	//var url = 
+	var url = 'https://api.github.com/gists/public';
+
+	var params = {
+		mode: 'json'
+
+	};
+	url += '?' + appendToQuery(params);
+	req.onreadystatechange = function() {
+		if (this.readyState === 4) {
+			var gistArray = JSON.parse(this.responseText);
+
+
+		}
+	};
+
+	//return gistArray
 }
 
-function saveDemoInput() {
-	localStorage.setItem('demoText',document.getElementsByName('demo-input')[0].value);
+
+
+function getGists() {
+	// put values from html page into javascript variables
+	var numPages = document.getElementsByName('numPages')[0].value;
+	var python = document.getElementsByName('python')[0].checked;
+	var javascript = document.getElementsByName('javascript')[0].checked;
+	var sql = document.getElementsByName('sql')[0].checked;
+	var jason = document.getElementsByName('json')[0].checked;
+
+	var gistArray = gitAjax(numPages, python, javascript, sql, jason);
 }
+
 
 function clearLocalStorage() {
 	localStorage.clear();
 }
 
-function displayLocalStorage(fav) {
-	document.getElementById('output').innerHTML = localStorage.getItem('demoText');
-}
+// function displayLocalStorage(fav) {
+//	document.getElementById('output').innerHTML = localStorage.getItem('demoText');
+// }
 
 window.onload = function() {
-	testFunc("Gwalla gwalla");
-	saveDemoInput();
-	displayLocalStorage("swag");
-	gistAjax();
+
 };
