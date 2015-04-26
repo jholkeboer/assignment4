db70d2fff07a683fc7f9e9ac33a4539a2fe25207
jholkeboer <jholkeboer@users.noreply.github.com>
2015-04-25 23:07:54 -0700

    gists showing up

--- assignment4.js
+++ assignment4.js
@@ -1,7 +1,102 @@
 //assignment4.js
 //jack holkeboer
 
-function printGists() {
+var gistStorage = null;
+var gistArray = [];
+var gistJSON = [];
+var numPages = 1;
+
+
+function Gist(desc, address) {
+	if (desc === null || desc == '' || desc === undefined) {
+		this.desc = "[no description]";
+	}
+	else {
+		this.desc = desc;
+	}
+	this.address = address;
+}
+
+// function addGist(gistStorage, gist) {
+// 	if (gist instanceof Gist) {
+// 		gistStorage.gists.push(gist);
+// 		localStorage.setItem('myGists', JSON.stringify(gistStorage))
+// 	}
+// 	console.error('That\'s not a gist.');
+// 	return false;
+// }
+
+
+function tableRow(gist) {
+	var tr = document.createElement('tr');
+	var td1 = document.createElement('td');
+	var td2 = document.createElement('td');
+	td1.innerHTML = '<a href=\"' + gist.address + '\">' + gist.desc + '</a>';
+	td2.innerHTML = 'button';
+	tr.appendChild(td1);
+	tr.appendChild(td2);
+	return tr;
+}
+
+// function extractGist(gist) {
+// 	var gistCell = document.createElement('td');
+// 	var buttonCell = document.createElement('td');
+// 	gistCell.innerHTML = '<a href=\"' + gist.address + '\">' + gist.desc + '</a>';
+// 	buttonCell = 'button';
+// 	return {'gistCell':gistCell, 'buttonCell':buttonCell};
+// }
+
+// function saveOptions() {
+
+// }
+
+function renderGistTable(table, gistJSON) {
+	//clear table
+	for (var i=table.childNodes.length - 1; i >= 0; i--) {
+		table.removeChild(table.childNodes[i]);
+	}
+	for (var j=0; j < gistJSON.length; j++) {
+		//loop through each page of gists (subarray)
+		for (var k=0; k < gistJSON[j].length; k++) {
+			var newGist = new Gist(gistJSON[j][k].description, gistJSON[j][k].html_url);
+			table.appendChild(tableRow(newGist));
+		}
+	}
+}
+
+
+//runs AJAX query
+function getGists() {
+	if (document.getElementsByName('numPages')[0].value >= 1 && document.getElementsByName('numPages')[0].value <= 5) {
+		var numPages = document.getElementsByName('numPages')[0].value;
+		gistJSON = [];
+		for (var i = 0; i < numPages; i++) {
+			var req = new XMLHttpRequest();
+			if (!req) {
+				throw 'Request did not go through.';
+			}
+			var url = 'https://api.github.com/gists/public';
+			var params = {
+				mode: 'json',
+				page: i + 1
+			};
+			url += '?' + appendToQuery(params);
+			console.log(url);
+			req.onreadystatechange = function() {
+				if (this.readyState === 4) {
+					gistJSON.push(JSON.parse(this.responseText));
+					console.log(gistJSON);
+				}
+				renderGistTable(document.getElementById('gist-table'), gistJSON);
+			};
+			req.open('GET', url);
+			req.send();
+		}
+	}
+	//invalid input case
+	else {
+		console.log('Invalid input.');
+	}
 
 }
 
@@ -15,51 +110,20 @@ function appendToQuery(params) {
 	return queryPiece.join('&');
 }
 
-function gistAjax(numPages, python, javascript, sql, jason) {
-	var req = new XMLHttpRequest();
-	if (!req) {
-		throw 'Request did not go through.';
-	}
-	var url = 'https://api.github.com/gists/public';
-
-	var params = {
-		mode: 'json'
-
-	};
-	url += '?' + appendToQuery(params);
-	req.onreadystatechange = function() {
-		if (this.readyState === 4) {
-			var gistArray = JSON.parse(this.responseText);
-
-
-		}
-	};
-
-	//return gistArray
-}
-
-
-
-function getGists() {
-	// put values from html page into javascript variables
-	var numPages = document.getElementsByName('numPages')[0].value;
-	var python = document.getElementsByName('python')[0].checked;
-	var javascript = document.getElementsByName('javascript')[0].checked;
-	var sql = document.getElementsByName('sql')[0].checked;
-	var jason = document.getElementsByName('json')[0].checked;
-
-	var gistArray = gitAjax(numPages, python, javascript, sql, jason);
-}
-
 
 function clearLocalStorage() {
 	localStorage.clear();
 }
 
-// function displayLocalStorage(fav) {
-//	document.getElementById('output').innerHTML = localStorage.getItem('demoText');
-// }
 
 window.onload = function() {
-
+	var testStorage = localStorage.getItem('myGists');
+	if (testStorage === null) {
+		gistStorage = {'gists': [], 'favs': []};
+		localStorage.setItem('myGists', JSON.stringify(gistStorage));
+	}
+	else {
+		gistStorage = JSON.parse(localStorage.getItem('myGists'));
+	}
+	getGists();
 };
