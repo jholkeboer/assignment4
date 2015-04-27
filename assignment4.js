415e5caa7e66298a4b38ad446fd4fd7fb5a18002
jholkeboer <jholkeboer@users.noreply.github.com>
2015-04-26 23:26:15 -0700

    working on localstorage

--- assignment4.js
+++ assignment4.js
@@ -4,10 +4,11 @@
 var gistArray = [];
 var gistJSON = [];
 var numPages = 1;
+var favStorage = null;
 
 
 function Gist(desc, address, lang) {
-	if (desc === null || desc == '' || desc === undefined) {
+	if (desc === null || desc === '' || desc === undefined) {
 		this.desc = "[no description]";
 	}
 	else {
@@ -23,23 +24,22 @@ function Fav(favLink, favLang) {
 	this.favLang = favLang;
 }
 
-function addFav(index) {
-	var tr = document.getElementById(index);
-	var favTable = document.getElementById('favs-table');
-	tr.childNodes[2].innerHTML = '<input type=\'button\' value=\'Delete\' onclick=\'removeFav(' + (tr.getAttribute("id")) + ')\'>';
-	favTable.appendChild(tr);
-
-	//remove element from gistJSON
-
+function addFav(favStorage, idCounter) {
+	var el = document.getElementById(idCounter);
+	var td1 = el.childNodes[0];
+	var td2 = el.childNodes[1];
+	var fav = new Fav(td1, td2);
+	if (fav instanceof Fav) {
+		favStorage.favs.push(fav);
+		localStorage.setItem('favStorageItem', JSON.stringify(favStorage));
+	}
+	console.error('fav add error');
+	return false;
 }
 
+
 function removeFav(index) {
-	var favTable = document.getElementById('favs-table');
-	var tr = favTable.childNodes[index];
-	var gistTable = document.getElementById('gist-table');
-	var newtr = tr;
-	//newtr.setAttribute("id", gistTable.childNodes.length);
-	gistTable.appendChild(newtr);
+
 }
 
 
@@ -66,43 +66,43 @@ function renderGistTable(table, gistJSON) {
 		table.removeChild(table.childNodes[i]);
 	}
 	var idCounter = 0;
+	favTable = document.getElementById('favs-table');
 	for (var j=0; j < gistJSON.length; j++) {
 		//loop through each page of gists (subarray)
 		for (var k=0; k < gistJSON[j].length; k++) {
-			var lang;
-			for (var key in gistJSON[j][k].files) {
-				if (gistJSON[j][k].files.hasOwnProperty(key)) {
-					if (gistJSON[j][k].files[key].language !== undefined && gistJSON[j][k].files[key].language != '' && gistJSON[j][k].files[key].language !== null) {
-						lang = gistJSON[j][k].files[key].language;
+				var lang;
+				for (var key in gistJSON[j][k].files) {
+					if (gistJSON[j][k].files.hasOwnProperty(key)) {
+						if (gistJSON[j][k].files[key].language !== undefined && gistJSON[j][k].files[key].language !== '' && gistJSON[j][k].files[key].language !== null) {
+							lang = gistJSON[j][k].files[key].language;
+						}
+						else {
+							lang = 'no language listed';
+						}
 					}
-					else {
-						lang = 'no language listed';
+				}
+				var newGist = new Gist(gistJSON[j][k].description, gistJSON[j][k].html_url, lang);
+				//language filtration
+				if (document.getElementsByName('python')[0].checked) {
+					if (newGist.lang == 'Python') {
+						continue;
 					}
 				}
-			}
-			var newGist = new Gist(gistJSON[j][k].description, gistJSON[j][k].html_url, lang);
-			//language filtration
-			if (document.getElementsByName('python')[0].checked) {
-				if (newGist.lang == 'Python') {
-					continue;
+				else if (document.getElementsByName('javascript')[0].checked) {
+					if (newGist.lang == 'JavaScript') {
+						continue;
+					}
 				}
-			}
-			else if (document.getElementsByName('javascript')[0].checked) {
-				if (newGist.lang == 'JavaScript') {
-					continue;
+				else if (document.getElementsByName('sql')[0].checked) {
+					if (newGist.lang == 'SQL') {
+						continue;
+					}
 				}
-			}
-			else if (document.getElementsByName('sql')[0].checked) {
-				if (newGist.lang == 'SQL') {
-					continue;
+				else if (document.getElementsByName('json')[0].checked) {
+					if (newGist.lang == 'JSON') {
+						continue;
+					}
 				}
-			}
-			else if (document.getElementsByName('json')[0].checked) {
-				if (newGist.lang == 'JSON') {
-					continue;
-				}
-			}
-
 			console.log(newGist);
 			table.appendChild(tableRow(newGist, idCounter));
 			idCounter += 1;
@@ -110,6 +110,24 @@ function renderGistTable(table, gistJSON) {
 	}
 }
 
+function renderFavTable(table) {
+	for (var i=table.childNodes.length - 1; i >= 0; i--) {
+		table.removeChild(table.childNodes[i]);
+	}
+	favStorage.favs.forEach(function(e) {
+		var tr = document.createElement('tr');
+		var td1 = document.createElement('td');
+		var td2 = document.createElement('td');
+		var td3 = document.createElement('td');
+		td1 = e.favLink;
+		td2 = e.favLang;
+		td3 = 
+		tr.appendChild(td1);
+		tr.appendChild(td2);
+		tr.appendChild(td3);
+		table.appendChild(tr);
+	});
+}
 
 //runs AJAX query
 function getGists() {
@@ -135,6 +153,7 @@ function getGists() {
 					gistJSON.push(JSON.parse(this.responseText));
 					console.log(gistJSON);
 				}
+
 				//render table
 				renderGistTable(document.getElementById('gist-table'), gistJSON);
 			};
@@ -168,12 +187,13 @@ function clearLocalStorage() {
 
 
 window.onload = function() {
-	var testStorage = localStorage.getItem('myGists');
+	var testStorage = localStorage.getItem('favStorageItem');
 	if (testStorage === null) {
-		gistStorage = {'favs': []};
-		localStorage.setItem('myGists', JSON.stringify(gistStorage));
+		favStorage = {'favs': []};
+		localStorage.setItem('favStorageItem', JSON.stringify(favStorage));
 	}
 	else {
-		gistStorage = JSON.parse(localStorage.getItem('myGists'));
+		favStorage = JSON.parse(localStorage.getItem('favStorageItem'));
 	}
+	renderFavTable(document.getElementById('favs-table'));
 };
